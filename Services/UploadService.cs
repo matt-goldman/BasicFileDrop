26d95e48d895e44e4b6e20636a54f47e5a09f4b0
Matt Goldman <matt.goldman@outlook.com>
2024-07-12 18:52:36 +1000

    Initial commit

---
+++ Services/UploadService.cs
@@ -0,0 +1,32 @@
+﻿namespace BasicFileDrop.Services;
+
+public class UploadService
+{
+    private readonly IWebHostEnvironment _environment;
+
+    public UploadService(IWebHostEnvironment environment)
+    {
+        _environment = environment;
+    }
+
+    public async Task<string> Upload(string fileName, Stream fileData)
+    {
+        if (fileName == null || fileData.Length == 0)
+            return "No file uploaded.";
+
+        var uploadPath = Path.Combine(_environment.ContentRootPath, "UploadedFiles");
+        if (!Directory.Exists(uploadPath))
+        {
+            Directory.CreateDirectory(uploadPath);
+        }
+
+        var filePath = Path.Combine(uploadPath, fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+            await fileData.CopyToAsync(stream);
+        }
+
+        return filePath;
+    }
+}
